6f8b632eb3d6e966e0dc650aa2424dd916e842a6
NISCHAY CHHETRI <chhetrinischay3@gmail.com>
2026-05-12 17:53:12 +0530

    Initialize Flask app and import necessary modules

--- apps.py
+++ apps.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template, request, redirect, session
+import matplotlib
+matplotlib.use('Agg')   
+import matplotlib.pyplot as plt
+from models import create_tables, connect_db
+import time
+from collections import defaultdict
+from datetime import datetime
 
+
+app = Flask(__name__)
+app.secret_key = "secret123"
+
+create_tables()
